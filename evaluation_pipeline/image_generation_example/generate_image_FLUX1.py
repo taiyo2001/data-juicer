ded1e39191764ca0813ff503745168bc4714cf17
taiyo2001 <g2481424@tcu.ac.jp>
2025-10-31 13:04:18 +0000

    feat: FLUX.1

---
+++ evaluation_pipeline/image_generation_example/generate_image_FLUX1.py
@@ -0,0 +1,84 @@
+import os
+import torch
+from diffusers import DiffusionPipeline
+import json
+import tqdm
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, default=None)
+    parser.add_argument('--model_name', type=str, default="SD1_5")
+    parser.add_argument('--prompt_path', type=str, default="./DetailMaster_Dataset/DetailMaster_Dataset.json")
+    parser.add_argument('--output_json', type=str, default="./output.json")
+    parser.add_argument('--image_output_dir', type=str, default="./output_image/")
+    parser.add_argument('--icl_num', type=str, default=None)
+    parser.add_argument('--icl_prompt', type=str, default=None)
+
+    args = parser.parse_args()
+
+    if 'dev' in args.model_name:
+        args.model_path = "black-forest-labs/FLUX.1-dev"
+    elif 'schnell' in args.model_name:
+        args.model_path = "black-forest-labs/FLUX.1-schnell"
+
+    args.output_json = f"./evaluation_pipeline/image_generation_example/output_image_info_{args.model_name}.json"
+    args.image_output_dir = f"./evaluation_pipeline/image_generation_example/output_image_{args.model_name}/"
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # GPU不足のためto("cuda")の移行はなし（遅いので移行する）
+    pipe = DiffusionPipeline.from_pretrained(
+        args.model_path,
+        # torch_dtype=torch.bfloat16,
+        load_in_4bit=True,
+    ).to("cuda")
+
+
+    new_data = []
+
+    if args.image_output_dir:
+        os.makedirs(args.image_output_dir, exist_ok=True)
+
+    with open(args.prompt_path, "r") as f:
+        data = json.load(f)
+
+    valid_image_count = 0
+    for temp_piece in tqdm.tqdm(data):
+        try:
+            prompt = temp_piece["polished_prompt"]
+            # print(f"--- before prompt {prompt} ---")
+            if args.icl_prompt and args.icl_num:
+                prompt = args.icl_prompt + "\n" + prompt
+            # print(f"--- after prompt {prompt} ---")
+            image = pipe(
+                prompt,
+                height=512,
+                width=512,
+                num_inference_steps=50,
+            ).images[0]
+
+            image_name = f"{temp_piece['dataset_target']}_{args.model_name}_{valid_image_count}_{temp_piece['image_id']}"
+            image.save(os.path.join(args.image_output_dir, image_name))
+
+            temp_json = {}
+            temp_json["output_image_name"] = image_name
+            temp_json["image_id"] = temp_piece["dataset_target"] + "_" + temp_piece["image_id"]
+            new_data.append(temp_json)
+
+            valid_image_count += 1
+
+        except Exception as e:
+            print(f"\n--- ERROR encountered for prompt {temp_piece['image_id']} ---")
+            print(e)
+            print("----------------------------------------------------------")
+        # except:
+        #     continue
+
+    with open(args.output_json, "a") as f:
+        json.dump(new_data, f)
--- exec.sh
+++ exec.sh
@@ -1,3 +1,15 @@
+# default settings
+ICL_NUM=1
+ICL_PROMPT="Always prioritize the spatial relationships and object count specified in the prompt."
+
+ICL_NUM=2
+ICL_PROMPT="Let's generate step by step"
+
+if [ -n "$ICL_NUM" ] && [ -n "$ICL_PROMPT" ]; then
+    MODEL_NAME="${MODEL_NAME}_ICL${ICL_NUM}"
+fi
+echo "FINAL MODEL_NAME: $MODEL_NAME"
+
 # ------ SD1.5 ------
 # generate
 # NOTE: 'conda activate DetailMaster' is required before. Exec from data-juicer/evaluation_pipeline/image_generation_example/
@@ -13,26 +25,17 @@ CUDA_VISIBLE_DEVICES=1 python evaluation_pipeline/eval_process.py \
 --output_log_dir ./playground/evaluation \
 --output_name_prefix SD1_5
 # calculate final eval metrics(No GPU)
-CUDA_VISIBLE_DEVICES=0 python evaluation_pipeline/cal_eval.py \
+python evaluation_pipeline/cal_eval.py \
 --eval_output_log_dir_name ./playground/evaluation \
 --name_prefix SD1_5
 
 
 # ------ ParaDiffusion ------
-# default settings
-ICL_NUM=1
-ICL_PROMPT="Always prioritize the spatial relationships and object count specified in the prompt."
-
 MODEL_NAME=ParaDiffusion
-
-if [ -n "$ICL_NUM" ] && [ -n "$ICL_PROMPT" ]; then
-    MODEL_NAME="${MODEL_NAME}_ICL${ICL_NUM}"
-fi
-echo "FINAL MODEL_NAME: $MODEL_NAME"
 
 # generate
 # NOTE: 'conda activate $MODEL_NAME' is required before
-CUDA_VISIBLE_DEVICES=0 python evaluation_pipeline/image_generation_example/generate_image_$MODEL_NAME.py \
+CUDA_VISIBLE_DEVICES=1 python evaluation_pipeline/image_generation_example/generate_image_ParaDiffusion.py \
 --model_name $MODEL_NAME \
 --icl_num $ICL_NUM \
 --icl_prompt $ICL_PROMPT
@@ -47,3 +50,18 @@ CUDA_VISIBLE_DEVICES=0 python evaluation_pipeline/eval_process.py \
 python evaluation_pipeline/cal_eval.py \
 --eval_output_log_dir_name ./playground/evaluation/$MODEL_NAME \
 --name_prefix $MODEL_NAME
+
+
+# ------ FLUX ------
+MODEL_NAME=FLUX1-dev
+MODEL_NAME=FLUX1-schnell
+
+# generate
+# NOTE: 'conda activate DetailMaster' is required before
+CUDA_VISIBLE_DEVICES=0 python evaluation_pipeline/image_generation_example/generate_image_FLUX1.py \
+--model_name $MODEL_NAME \
+--icl_num $ICL_NUM \
+--icl_prompt $ICL_PROMPT
+
+
+# ------ LongAlign ------
