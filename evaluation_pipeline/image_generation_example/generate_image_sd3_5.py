6bf46fa4b4aa177700f72952a766d2cfbbbeaa75
taiyo2001 <g2481424@tcu.ac.jp>
2026-01-25 21:29:23 +0900

    fix: add dense prompt

--- evaluation_pipeline/image_generation_example/generate_image_sd3_5.py
+++ evaluation_pipeline/image_generation_example/generate_image_sd3_5.py
@@ -13,6 +13,7 @@ from diffusers import StableDiffusion3Pipeline, BitsAndBytesConfig, SD3Transform
 from transformers import T5EncoderModel
 from sd_embed.src.sd_embed.embedding_funcs import get_weighted_text_embeddings_sd3
 from src.services.slack_client_service import slack_service, build_image_generation_start_message, build_image_generation_complete_message
+from src.utils.env_detector import check_is_colab
 from src.constants import DETAIL_MASTER
 import json
 import tqdm
@@ -35,17 +36,36 @@ def parse_args():
     parser.add_argument('--np_num', type=str, default=None)
     parser.add_argument('--np_prompt', type=str, default=None)
     parser.add_argument('--np_prompt_path', type=str, default=None)
+    # dense prompt
+    parser.add_argument('--dp_num', type=str, default=None)
+    parser.add_argument('--dp_prompt_path', type=str, default=None)
+    parser.add_argument('--disable_dp_clip', action='store_false', dest='dp_clip')
+    parser.add_argument('--disable_dp_t5', action='store_false', dest='dp_t5')
+
+    # default dense prompt adaption to both
+    parser.set_defaults(dp_clip=True, dp_t5=True)
 
     args=parser.parse_args()
 
+    # in-context learning
     if args.sp_num is not None and args.sp_prompt is not None:
         if args.sp_t5_only:
             args.model_name = args.model_name + f"_T5_SP{args.sp_num}"
         else:
             args.model_name = args.model_name + f"_SP{args.sp_num}"
 
+    # negative prompt
     if args.np_num is not None and (args.np_prompt is not None or args.np_prompt_path is not None):
         args.model_name = args.model_name + f"_NP{args.np_num}"
+
+    # dense prompt
+    if args.dp_num is not None and args.dp_prompt_path is not None:
+        if args.dp_clip and args.dp_t5:
+            args.model_name = args.model_name + f"_DP{args.dp_num}"
+        elif args.dp_clip:
+            args.model_name = args.model_name + f"_DP{args.dp_num}-C"
+        elif args.dp_t5:
+            args.model_name = args.model_name + f"_DP{args.dp_num}-T"
 
     if args.count is not None:
         args.model_name = args.model_name + f"_{args.count}"
@@ -91,15 +111,9 @@ if __name__ == "__main__":
     elif '_SL512' in args.model_name:
         max_sequence_length = 512
 
-    # --- Colab Configuration ---
-    try:
-        import google.colab
-        is_colab = True
-    except:
-        is_colab = False
-    print(f"--- is_colab: {is_colab} ---")
-    # ----------------------------
+    is_colab = check_is_colab()
 
+    print(f"--- is_colab: {is_colab} ---")
     print("--- Model Name: ", args.model_name, " ---")
     print("--- Prompt Weighting: ", prompt_weighting, ", CLIP: ", extend_clip, ", T5: ", exted_t5, " ---")
     print("--- Max Sequence Length: ", max_sequence_length, " ---")
@@ -107,6 +121,9 @@ if __name__ == "__main__":
     print("--- SP Prompt: ", args.sp_prompt, " ---")
     print("--- Negative Prompt Num: ", args.np_num, " ---")
     print("--- Negative Prompt: ", args.np_prompt_path or args.np_prompt, " ---")
+    print("--- Dense Prompt Num: ", args.dp_num, " ---")
+    print("--- Dense Prompt: ", args.dp_prompt_path, " ---")
+    print(f"--- Dense Prompt Adaption: CLIP: {args.dp_clip}, T5: {args.dp_t5} ---")
 
     # notify start
     mention_id = os.environ.get("SLACK_MENTION_ID")
@@ -117,6 +134,9 @@ if __name__ == "__main__":
         "SP Prompt": args.sp_prompt,
         "Negative Prompt Num": args.np_num,
         "Negative Prompt": args.np_prompt_path or args.np_prompt,
+        "Dense Prompt Num": args.dp_num,
+        "Dense Prompt": args.dp_prompt_path,
+        "Dense Prompt Adaption": f"CLIP: {args.dp_clip}, T5: {args.dp_t5}",
     }
     message = build_image_generation_start_message(args.model_name, model_info)
     slack_message_ts_start = slack_service.send_message(message=message, mention_id=mention_id)
@@ -130,7 +150,7 @@ if __name__ == "__main__":
         ).to("cuda")
     else:
         # WARN: OOM in 4bit model on 24GB TITAN RTX GPU
-        nf4_config = BitsAndBytesConfig(
+        quantization_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
             bnb_4bit_compute_dtype=torch.bfloat16
@@ -138,7 +158,7 @@ if __name__ == "__main__":
         model_nf4 = SD3Transformer2DModel.from_pretrained(
             args.model_path,
             subfolder="transformer",
-            quantization_config=nf4_config,
+            quantization_config=quantization_config,
             torch_dtype=torch.bfloat16
         ).to("cuda")
         t5_nf4 = T5EncoderModel.from_pretrained(
@@ -158,6 +178,7 @@ if __name__ == "__main__":
     if args.image_output_dir:
         os.makedirs(args.image_output_dir, exist_ok=True)
 
+    # load prompt dict
     if args.np_prompt_path:
         with open(args.np_prompt_path, "r") as f:
             np_data = json.load(f)
@@ -170,9 +191,24 @@ if __name__ == "__main__":
                 np_dict[dict_image_id] = dict_np
         print("np_dict len: ", len(np_dict))
 
+    if args.dp_prompt_path:
+        with open(args.dp_prompt_path, "r") as f:
+            dp_data = json.load(f)
+
+        dp_dict = {}
+        for item in dp_data:
+            dict_image_id = item.get("image_id")
+            dict_dp = item.get("llm_output")
+            if dict_image_id and dict_dp:
+                dp_dict[dict_image_id] = dict_dp
+        print("dp_dict len: ", len(dp_dict))
+
+
     with open(args.prompt_path, "r") as f:
         data = json.load(f)
 
+    total_count = len(data)
+    report_step = max(1, total_count // 10)
     valid_image_count = 0
     for temp_piece in tqdm.tqdm(data):
         try:
@@ -186,13 +222,38 @@ if __name__ == "__main__":
             elif args.np_prompt and args.np_num:
                 neg_prompt = args.np_prompt
 
+            dense_prompt = None
+            if args.dp_prompt_path and args.dp_num:
+                dense_prompt = dp_dict.get(image_id)
+                if dense_prompt is not None:
+                    print("Dense Prompt: ", dense_prompt[:50], "...")
+                else:
+                    print("No Dense Prompt found for ", image_id)
+
             if prompt_weighting:
+                # normal
                 prompt = normal_prompt
                 llm_prompt = None
-                if args.sp_prompt and args.sp_num and args.sp_t5_only:
-                    llm_prompt = sp_positive_prompt
-                elif args.sp_prompt and args.sp_num:
-                    prompt = sp_positive_prompt
+                # -- SP ---
+                if args.sp_prompt and args.sp_num:
+                    if args.sp_t5_only:
+                        llm_prompt = sp_positive_prompt
+                    else:
+                        prompt = sp_positive_prompt
+                # -- DP ---
+                if args.dp_prompt_path and args.dp_num and dense_prompt is not None:
+                    if args.dp_clip and args.dp_t5:
+                        print("Adapt Dense Prompt Both CLIP and T5")
+                        prompt = dense_prompt
+                        llm_prompt = dense_prompt
+                    elif args.dp_clip:
+                        print("Adapt Dense Prompt CLIP")
+                        prompt = dense_prompt
+                        llm_prompt = normal_prompt
+                    elif args.dp_t5:
+                        print("Adapt Dense Prompt T5")
+                        prompt = normal_prompt
+                        llm_prompt = dense_prompt
 
                 (prompt_embeds, prompt_neg_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds) = get_weighted_text_embeddings_sd3(
                     pipe,
@@ -248,10 +309,22 @@ if __name__ == "__main__":
 
             valid_image_count += 1
 
+            # --- noti progress ---
+            if valid_image_count % report_step == 0 and valid_image_count < total_count:
+                percentage = (valid_image_count / total_count) * 100
+                progress_message = (
+                    f" :hourglass_flowing_sand: 進捗報告: {percentage:.0f}% 完了 "
+                    f"({valid_image_count}/{total_count})\n"
+                )
+                slack_service.send_message(message=progress_message, thread_ts=slack_message_ts_start)
+
         except Exception as e:
             print(f"\n--- ERROR encountered for prompt {temp_piece['image_id']} ---")
             print(e)
             print("----------------------------------------------------------")
+            message = f":warning: ({valid_image_count})回目でエラーが発生しました: {e} for prompt ID {temp_piece['image_id']}"
+            slack_message_ts_complete = slack_service.send_message(message=message, thread_ts=slack_message_ts_start)
+            valid_image_count += 1
         # except:
         #     continue
 
