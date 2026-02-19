abb677a70ab711d5adfcb85d75dd7bb2b3d31749
taiyo2001 <g2481424@tcu.ac.jp>
2026-02-20 01:39:25 +0900

    fix: FLUX.1

--- evaluation_pipeline/image_generation_example/generate_image_FLUX1.py
+++ evaluation_pipeline/image_generation_example/generate_image_FLUX1.py
@@ -18,6 +18,8 @@ GGUF_MODEL_PATH = None
 import torch
 from diffusers import DiffusionPipeline, FluxPipeline, FluxTransformer2DModel, GGUFQuantizationConfig
 from sd_embed.src.sd_embed.embedding_funcs import get_weighted_text_embeddings_flux1
+from src.services.slack_client_service import slack_service, build_image_generation_start_message, build_image_generation_complete_message
+from src.utils.env_detector import check_is_colab
 from src.constants import DETAIL_MASTER
 import json
 import tqdm
@@ -33,6 +35,7 @@ def parse_args():
     parser.add_argument('--output_json', type=str, default="./output.json")
     parser.add_argument('--image_output_dir', type=str, default="./output_image/")
     parser.add_argument('--count', type=str, default=None)
+    parser.add_argument('--save_start_threshold', type=int, default=None)
     # in-context learning
     parser.add_argument('--sp_num', type=str, default=None)
     parser.add_argument('--sp_prompt', type=str, default=None)
@@ -40,17 +43,44 @@ def parse_args():
     # negative prompt
     parser.add_argument('--np_num', type=str, default=None)
     parser.add_argument('--np_prompt', type=str, default=None)
+    parser.add_argument('--np_prompt_path', type=str, default=None)
+    # dense prompt
+    parser.add_argument('--dp_num', type=str, default=None)
+    parser.add_argument('--dp_prompt_path', type=str, default=None)
+    parser.add_argument('--disable_dp_clip', action='store_false', dest='dp_clip')
+    parser.add_argument('--disable_dp_t5', action='store_false', dest='dp_t5')
+    # clip prompt
+    parser.add_argument('--cp_num', type=str, default=None)
+    parser.add_argument('--cp_prompt_path', type=str, default=None)
+
+    # default dense prompt adaption to both
+    parser.set_defaults(dp_clip=True, dp_t5=True)
 
     args = parser.parse_args()
 
+    # in-context learning
     if args.sp_num is not None and args.sp_prompt is not None:
         if args.sp_t5_only:
             args.model_name = args.model_name + f"_T5_SP{args.sp_num}"
         else:
             args.model_name = args.model_name + f"_SP{args.sp_num}"
 
-    if args.np_num is not None and args.np_prompt is not None:
+    # negative prompt
+    if args.np_num is not None and (args.np_prompt is not None or args.np_prompt_path is not None):
         args.model_name = args.model_name + f"_NP{args.np_num}"
+
+    # dense prompt
+    if args.dp_num is not None and args.dp_prompt_path is not None:
+        if args.dp_clip and args.dp_t5:
+            args.model_name = args.model_name + f"_DP{args.dp_num}"
+        elif args.dp_clip:
+            args.model_name = args.model_name + f"_DP{args.dp_num}-C"
+        elif args.dp_t5:
+            args.model_name = args.model_name + f"_DP{args.dp_num}-T"
+
+    # clip prompt
+    if args.cp_num is not None and args.cp_prompt_path is not None:
+        args.model_name = args.model_name + f"_CP{args.cp_num}"
 
     if args.count is not None:
         args.model_name = args.model_name + f"_{args.count}"
@@ -75,36 +105,53 @@ if __name__ == "__main__":
 
     # NOTE: FLUX.1はdevのみprompt weighting対応（効果はイマイチらしい）
     prompt_weighting = False
+    extend_clip = False
+    exted_t5 = False
     if "_EM" in args.model_name:
-        if 'schnell' in args.model_name:
-            print("!!! WARNING: FLUX.1-schnell does not support prompt weighting. Disabled prompt weighting. !!!")
-            exit(1)
-
         prompt_weighting = True
+        if '_EM-C' in args.model_name:
+            extend_clip = True
+        elif '_EM-T' in args.model_name:
+            exted_t5 = True
+        elif '_EM-A' in args.model_name:
+            extend_clip = True
+            exted_t5 = True
 
     max_sequence_length = 256 # default
     if '_SL256' in args.model_name:
         max_sequence_length = 256
     elif '_SL512' in args.model_name:
         max_sequence_length = 512
+    elif '_SL0' in args.model_name:
+        max_sequence_length = None
 
-    # --- Colab Configuration ---
-    try:
-        import google.colab
-        is_colab = True
-        DRIVE_PATH_BASE = '/content/drive/MyDrive/workspace/huggingface_cache/'
-    except:
-        is_colab = False
+    is_colab = check_is_colab()
+
     print(f"--- is_colab: {is_colab} ---")
-    # ----------------------------
-
     print("--- Model Name: ", args.model_name, " ---")
-    print("--- Prompt Weighting: ", prompt_weighting, " ---")
+    print("--- Prompt Weighting: ", prompt_weighting, ", CLIP: ", extend_clip, ", T5: ", exted_t5, " ---")
     print("--- Max Sequence Length: ", max_sequence_length, " ---")
-    print("--- SP Num: ", args.sp_num, " ---")
-    print("--- SP Prompt: ", args.sp_prompt, " ---")
-    print("--- Negative Prompt Num: ", args.np_num, " ---")
-    print("--- Negative Prompt: ", args.np_prompt, " ---")
+    print(f"--- System Prompt: No.{args.sp_num}-{args.sp_prompt}  ---")
+    print(f"--- Negative Prompt: No.{args.np_num}-{args.np_prompt_path or args.np_prompt}  ---")
+    print(f"--- Dense Prompt: No.{args.dp_num}-{args.dp_prompt_path}  ---")
+    print(f"--- Dense Prompt Adaption: CLIP: {args.dp_clip}, T5: {args.dp_t5} ---")
+    print(f"--- CLIP Prompt: No.{args.cp_num}-{args.cp_prompt_path}  ---")
+    print("--- SAVE START THRESHOLD: ", args.save_start_threshold, " ---")
+
+    # notify start
+    mention_id = os.environ.get("SLACK_MENTION_ID")
+    model_info = {
+        "Prompt Weighting": f"{prompt_weighting}, CLIP: {extend_clip}, T5: {exted_t5}",
+        "Max Sequence Length": max_sequence_length,
+        "System Prompt": f"No.{args.sp_num}-{args.sp_prompt}",
+        "Negative Prompt": f"No.{args.np_num}-{args.np_prompt_path or args.np_prompt}",
+        "Dense Prompt": f"No.{args.dp_num}-{args.dp_prompt_path}",
+        "Dense Prompt Adaption": f"CLIP: {args.dp_clip}, T5: {args.dp_t5}",
+        "CLIP Prompt": f"No.{args.cp_num}-{args.cp_prompt_path}",
+        "SAVE START THRESHOLD": args.save_start_threshold,
+    }
+    message = build_image_generation_start_message(args.model_name, model_info)
+    slack_message_ts_start = slack_service.send_message(message=message, mention_id=mention_id)
 
     if is_colab:
         pipe = DiffusionPipeline.from_pretrained(
@@ -126,68 +173,197 @@ if __name__ == "__main__":
         )
         # ).to("cuda")
 
-
     new_data = []
 
     if args.image_output_dir:
         os.makedirs(args.image_output_dir, exist_ok=True)
 
+    # load prompt dict
+    if args.np_prompt_path:
+        with open(args.np_prompt_path, "r") as f:
+            np_data = json.load(f)
+
+        np_dict = {}
+        for item in np_data:
+            dict_image_id = item.get("image_id")
+            dict_np = item.get("negative_prompt")
+            if dict_image_id and dict_np:
+                np_dict[dict_image_id] = dict_np
+        print("np_dict len: ", len(np_dict))
+
+    if args.dp_prompt_path:
+        with open(args.dp_prompt_path, "r") as f:
+            dp_data = json.load(f)
+
+        dp_dict = {}
+        for item in dp_data:
+            dict_image_id = item.get("image_id")
+            dict_dp = item.get("llm_output")
+            if dict_image_id and dict_dp:
+                dp_dict[dict_image_id] = dict_dp
+        print("dp_dict len: ", len(dp_dict))
+
+    if args.cp_prompt_path:
+        with open(args.cp_prompt_path, "r") as f:
+            cp_data = json.load(f)
+
+        cp_dict = {}
+        for item in cp_data:
+            dict_image_id = item.get("image_id")
+            dict_cp = item.get("llm_output")
+            if dict_image_id and dict_cp:
+                cp_dict[dict_image_id] = dict_cp
+        print("cp_dict len: ", len(cp_dict))
+
     with open(args.prompt_path, "r") as f:
         data = json.load(f)
 
+    total_count = len(data)
+    report_step = max(1, total_count // 10)
     valid_image_count = 0
     for temp_piece in tqdm.tqdm(data):
         try:
-            # SD3.5の効果検証で精度が悪くなかったらFLUXにも適用
-            # normal_prompt = temp_piece["polished_prompt"]
-            # sp_positive_prompt = args.sp_prompt + "\n" + normal_prompt
-
-            prompt = temp_piece["polished_prompt"]
-            if args.sp_prompt and args.sp_num:
-                prompt = args.sp_prompt + "\n" + prompt
+            image_id = f"{temp_piece['dataset_target']}_{temp_piece['image_id']}"
+            image_name = f"{temp_piece['dataset_target']}_{args.model_name}_{valid_image_count}_{temp_piece['image_id']}"
+            normal_prompt = temp_piece["polished_prompt"]
+            sp_positive_prompt = args.sp_prompt + "\n" + normal_prompt if args.sp_prompt and args.sp_num else normal_prompt
 
             neg_prompt = ""
-            if args.np_prompt and args.np_num:
+            if args.np_prompt_path and args.np_num:
+                neg_prompt = np_dict.get(image_id)
+            elif args.np_prompt and args.np_num:
                 neg_prompt = args.np_prompt
 
-            if prompt_weighting:
-                print("!!! WARNING: Prompt weighting for FLUX.1 is not yet implemented. Generating without prompt weighting. !!!")
-                # TODO: Search implementation for FLUX.1
-                # prompt_embeds, pooled_prompt_embeds = get_weighted_text_embeddings_flux1(
-                #     pipe  = pipe, prompt = prompt
-                # )
-                # image = pipe(
-                #     prompt_embeds,
-                #     pooled_prompt_embeds=pooled_prompt_embeds,
-                #     height=DETAIL_MASTER.IMAGE_SIZE.SMALL,
-                #     width=DETAIL_MASTER.IMAGE_SIZE.SMALL,
-                #     num_inference_steps=num_inference_steps,
-                # ).images[0]
-            else:
-                image = pipe(
-                    prompt,
-                    height=DETAIL_MASTER.IMAGE_SIZE.SMALL,
-                    width=DETAIL_MASTER.IMAGE_SIZE.SMALL,
-                    num_inference_steps=num_inference_steps, # schnell: 4, dev: 50
-                    max_sequence_length=max_sequence_length,
-                ).images[0]
+            dense_prompt = None
+            if args.dp_prompt_path and args.dp_num:
+                dense_prompt = dp_dict.get(image_id)
+                if dense_prompt is not None:
+                    print("Dense Prompt: ", dense_prompt[:50], "...")
+                else:
+                    print("No Dense Prompt found for ", image_id)
 
-            image_name = f"{temp_piece['dataset_target']}_{args.model_name}_{valid_image_count}_{temp_piece['image_id']}"
-            image.save(os.path.join(args.image_output_dir, image_name))
+            clip_prompt = None
+            if args.cp_prompt_path and args.cp_num:
+                clip_prompt = cp_dict.get(image_id)
+                if clip_prompt is not None:
+                    print("CLIP Prompt: ", clip_prompt[:50], "...")
+                else:
+                    print("No CLIP Prompt found for ", image_id)
+
+            if args.save_start_threshold is None or valid_image_count > args.save_start_threshold:
+                if prompt_weighting:
+                    # print("!!! WARNING: Prompt weighting for FLUX.1 is not yet implemented. Generating without prompt weighting. !!!")
+                    # TODO: Search implementation for FLUX.1
+                    # normal
+                    prompt = normal_prompt
+                    llm_prompt = None
+                    # -- SP ---
+                    if args.sp_prompt and args.sp_num:
+                        if args.sp_t5_only:
+                            llm_prompt = sp_positive_prompt
+                        else:
+                            prompt = sp_positive_prompt
+                    # -- DP ---
+                    if args.dp_prompt_path and args.dp_num and dense_prompt is not None:
+                        if args.dp_clip and args.dp_t5:
+                            print("Adapt Dense Prompt Both CLIP and T5")
+                            prompt = dense_prompt
+                            llm_prompt = dense_prompt
+                        elif args.dp_clip:
+                            print("Adapt Dense Prompt CLIP")
+                            prompt = dense_prompt
+                            llm_prompt = normal_prompt
+                        elif args.dp_t5:
+                            print("Adapt Dense Prompt T5")
+                            prompt = normal_prompt
+                            llm_prompt = dense_prompt
+                    # -- CP ---
+                    if args.cp_prompt_path and args.cp_num and clip_prompt is not None:
+                        print("Adapt CLIP Prompt")
+                        prompt = clip_prompt
+                        llm_prompt = normal_prompt
+
+                    prompt_embeds, pooled_prompt_embeds = get_weighted_text_embeddings_flux1(
+                        pipe,
+                        prompt=prompt,
+                        llm_prompt=llm_prompt,
+                        extend_clip=extend_clip,
+                        extend_t5=exted_t5,
+                        t5_max_length=max_sequence_length,
+                    )
+                    image = pipe(
+                        prompt_embeds,
+                        pooled_prompt_embeds=pooled_prompt_embeds,
+                        height=DETAIL_MASTER.IMAGE_SIZE.SMALL,
+                        width=DETAIL_MASTER.IMAGE_SIZE.SMALL,
+                        num_inference_steps=num_inference_steps,
+                        max_sequence_length=max_sequence_length,
+                    ).images[0]
+                else:
+                    prompt = normal_prompt
+                    if args.sp_prompt and args.sp_num:
+                        prompt = sp_positive_prompt
+
+                    if max_sequence_length is None:
+                        print("Using full sequence length for encoding prompts...!!")
+                        target_sequence_length = 768
+
+                        with torch.no_grad():
+                            prompt_embeds, pooled_prompt_embeds, _ = pipe.encode_prompt(
+                                prompt=prompt,
+                                prompt_2=prompt,
+                                max_sequence_length=target_sequence_length,
+                            )
+
+                        image = pipe(
+                            prompt_embeds=prompt_embeds,
+                            pooled_prompt_embeds=pooled_prompt_embeds,
+                            height=DETAIL_MASTER.IMAGE_SIZE.SMALL,
+                            width=DETAIL_MASTER.IMAGE_SIZE.SMALL,
+                            num_inference_steps=num_inference_steps,
+                        ).images[0]
+                    else:
+                        image = pipe(
+                            prompt,
+                            height=DETAIL_MASTER.IMAGE_SIZE.SMALL,
+                            width=DETAIL_MASTER.IMAGE_SIZE.SMALL,
+                            num_inference_steps=num_inference_steps, # schnell: 4, dev: 50
+                            max_sequence_length=max_sequence_length,
+                        ).images[0]
+
+                image.save(os.path.join(args.image_output_dir, image_name))
+            else:
+                print(f"Skipping image save for index {valid_image_count} (JSON entry only)")
 
             temp_json = {}
             temp_json["output_image_name"] = image_name
-            temp_json["image_id"] = temp_piece["dataset_target"] + "_" + temp_piece["image_id"]
+            temp_json["image_id"] = image_id
             new_data.append(temp_json)
 
             valid_image_count += 1
+
+            # --- noti progress ---
+            if valid_image_count % report_step == 0 and valid_image_count < total_count:
+                percentage = (valid_image_count / total_count) * 100
+                progress_message = (
+                    f" :hourglass_flowing_sand: 進捗報告: {percentage:.0f}% 完了 "
+                    f"({valid_image_count}/{total_count})\n"
+                )
+                slack_service.send_message(message=progress_message, thread_ts=slack_message_ts_start)
 
         except Exception as e:
             print(f"\n--- ERROR encountered for prompt {temp_piece['image_id']} ---")
             print(e)
             print("----------------------------------------------------------")
+            message = f":warning: ({valid_image_count})回目でエラーが発生しました: {e} for prompt ID {temp_piece['image_id']}"
+            slack_message_ts_complete = slack_service.send_message(message=message, thread_ts=slack_message_ts_start)
+            valid_image_count += 1
         # except:
         #     continue
 
     with open(args.output_json, "a") as f:
         json.dump(new_data, f)
+
+    # notify end
+    message = build_image_generation_complete_message(args.model_name)
+    slack_message_ts_complete = slack_service.send_message(message=message, thread_ts=slack_message_ts_start)
